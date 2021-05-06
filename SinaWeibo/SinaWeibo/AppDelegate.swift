54672959fbc31305ad76f625a818b80445767371
eaqcy22 <nidagedehao816@163.com>
2021-05-06 22:45:53 +0800

    添加tabbar及各个子控制器

--- SinaWeibo/SinaWeibo/AppDelegate.swift
+++ SinaWeibo/SinaWeibo/AppDelegate.swift
@@ -15,7 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame:UIScreen.main.bounds)
+        
+        window?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        let tabbarVC = ZTTabBarController()
+        
+        window?.rootViewController = tabbarVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ SinaWeibo/SinaWeibo/Classes/View/Main/Controller/ZTTabBarController.swift
@@ -0,0 +1,61 @@
+//
+//  ZTTabBarController.swift
+//  SinaWeibo
+//
+//  Created by sharayuki on 2017/1/4.
+//  Copyright © 2017年 sharayuki. All rights reserved.
+//
+
+import UIKit
+
+class ZTTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //添加子控制器
+       addChildViewControllers()
+        
+    }
+//添加子控制器方法
+    func addChildViewControllers() {
+        
+        addChildViewController(addChildVC(vc: ZTHomeTableViewController(), imgName: "tabbar_home", title: "首页"))
+        
+        addChildViewController(addChildVC(vc: ZTMessageTableViewController(), imgName: "tabbar_message_center", title: "消息"))
+        
+        addChildViewController(addChildVC(vc: ZTDiscoverTableViewController(), imgName: "tabbar_discover", title: "发现"))
+        
+        addChildViewController(addChildVC(vc: ZTProfileTableViewController(), imgName: "tabbar_profile", title: "我的"))
+    }
+//添加单个子控制器
+    func addChildVC(vc:UITableViewController,imgName:String,title:String) -> UINavigationController {
+        
+        let nav = UINavigationController(rootViewController: vc)
+        
+        vc.title = title
+        
+        vc.tabBarItem.image = UIImage(imageLiteralResourceName: imgName).withRenderingMode(.alwaysOriginal)
+        
+        vc.tabBarItem.selectedImage = UIImage(imageLiteralResourceName: imgName + "_selected").withRenderingMode(.alwaysOriginal)
+        
+        vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.orange], for: .selected)
+        
+        return nav
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
